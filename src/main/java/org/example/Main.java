a079b10c95f15607ec627ba9caded73eeee13359
kristinabusch <Kristina.Busch@sales-forum.de>
2023-08-24 18:30:26 +0200

    Testing

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Main {
+
+    public static void Main(String[] args) {
+
+    }
+    public static int calcSum(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+    public static boolean isEven(int a) {
+        return (a % 2) == 0;
+    }
+    public static int calc(int a, int b) {
+        return (a * b);
+    }
+}
