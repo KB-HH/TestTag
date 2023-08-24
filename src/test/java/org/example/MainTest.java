369608ee3d60f7e6dce9bb7e8ba5628c9007434e
kristinabusch <Kristina.Busch@sales-forum.de>
2023-08-24 18:33:19 +0200

    MainTest added

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.PackageUtils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    void return5_whenSumGets3And2() {
+        //GIVEN
+        int a = 3;
+        int b = 2;
+        // WHEN
+        int actual = Main.calcSum(a,b);
+        //THEN
+        int expected = 5;
+        assertEquals( expected, actual );
+    }
+
+    @Test
+    void returnFalse_whenIsEven_getsOddValue() {
+        //GIVEN
+        int a = 3;
+        // WHEN
+        boolean actual = Main.isEven( a );
+        //THEN
+        boolean expected = false;
+        assertFalse( actual );
+    }
+    @Test
+    void return_calc_Value() {
+        //GIVEN
+        int a = 3;
+        int b = 2;
+        // WHEN
+        int actual = Main.calc(a,b);
+        //THEN
+        int expected = 6;
+        assertEquals( expected, actual );
+    }
+}
